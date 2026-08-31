5fca00141790568fd5ac9af21cfcdd636c65a5d3
rapelsaja513-stack <rapelsaja513@gmail.com>
2026-08-31 20:43:12 +0700

    Update app.js

--- app.js
+++ app.js
@@ -15,14 +15,13 @@ import {
   ============================================
 */
 const firebaseConfig = {
-  apiKey: "GANTI_DENGAN_API_KEY",
-  authDomain: "GANTI_DENGAN_PROJECT_ID.firebaseapp.com",
-  projectId: "GANTI_DENGAN_PROJECT_ID",
-  storageBucket: "GANTI_DENGAN_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "GANTI_DENGAN_MESSAGING_SENDER_ID",
-  appId: "GANTI_DENGAN_APP_ID"
+  apiKey: "AIzaSyC6Vt_C-UM3BnvgBMnoitu5CBRHd3b_ikk",
+  authDomain: "pakkom-ecotrack.firebaseapp.com",
+  projectId: "pakkom-ecotrack",
+  storageBucket: "pakkom-ecotrack.firebasestorage.app",
+  messagingSenderId: "609321292317",
+  appId: "1:609321292317:web:3bf4e7d0c8ea6e861d2cf4"
 };
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const storage = getStorage(app);
